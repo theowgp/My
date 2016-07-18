1ad20ce35363e5dd0ace1a94220644b826e26451
myroslav <theowgp@gmail.com>
2016-07-18 11:15:08 +0200

    original ertur and koch model

--- ppp.m
+++ ppp.m
@@ -5,29 +5,28 @@ function  ppp(dynamics, n, eps)
 
 y1 = linspace(0 + eps, 1 - eps, n);
 y2 = linspace(0 + eps, 1 - eps, n);
-y3 = linspace(0 + eps, 1 - eps, n);
 
 
-[x, y, z] = meshgrid(y1, y2, y3);
 
-u = zeros(n, n, n);
-v = zeros(n, n, n);
-w = zeros(n, n, n);
+[x, y] = meshgrid(y1, y2);
+
+u = zeros(n, n);
+v = zeros(n, n);
+
 
 t=0;
-for i = 1:n*n*n
-    Yprime = dynamics.f(t, [x(i); y(i); z(i)]);
+for i = 1:n*n
+    Yprime = dynamics.f(t, [x(i); y(i)]);
     u(i) = Yprime(1);
     v(i) = Yprime(2);
-    w(i) = Yprime(3);
+
 end
 
 
-quiver3(x, y, z, u, v, w, 'r');
+quiver(x, y, u, v,'r');
 figure(gcf)
 xlabel('x_1')
 ylabel('x_2')
-zlabel('x_3')
 axis tight equal;
 
 end
--- ps.m
+++ ps.m
@@ -4,7 +4,6 @@ function  ps(dynamics, rk, n, eps)
 % eps = 0.05;
 y1 = linspace(0 + eps, 1 - eps, n);
 y2 = linspace(0 + eps, 1 - eps, n);
-y3 = linspace(0 + eps, 1 - eps, n);
 
 % %create a mesh grid(n*n)
 % [x, y] = meshgrid(y1, y2);
@@ -13,18 +12,18 @@ y3 = linspace(0 + eps, 1 - eps, n);
 %loop across all the elements of mesh grid.
 for i=1:n
     for j=1:n
-        for k=1:n
+        
 %             if(i~=j && j~=k)
                 % assign initial value
-                x0 = [y1(i), y2(j), y3(k)];
+                x0 = [y1(i), y2(j)];
                 % solve initial value problem
                 sol = rk.solve(x0,  @(t, x) dynamics.f(t, x));
                 % plot the solutions with respect to each other (2D)
-                plot3(sol(:, 1), sol(:, 2), sol(:, 3), 'LineWidth', 2);             % plot the state trajectory
-                plot3(sol(1, 1), sol(1, 2), sol(1, 3), 'bo', 'LineWidth', 3);       % starting point
-                plot3(sol(end, 1), sol(end, 2), sol(end, 3), 'ks', 'LineWidth', 3); % ending point
+                plot(sol(:, 1), sol(:, 2), 'LineWidth', 2);             % plot the state trajectory
+                plot(sol(1, 1), sol(1, 2), 'bo', 'LineWidth', 3);       % starting point
+                plot(sol(end, 1), sol(end, 2), 'ks', 'LineWidth', 3); % ending point
 %             end
-        end
+        
     end
         
 end
